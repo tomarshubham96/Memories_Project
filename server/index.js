506d45fd8eb38089df368428bc21bef27c3198f8
tomarshubham96 <tomarshubham14@stu.upes.ac.in>
2021-04-19 10:44:40 +0530

    Memories Project First Commit

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = "mongodb+srv://memoriesUser:demoPass@123@memoriescluster.nfso8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`)))
+    .catch((error) =>console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
